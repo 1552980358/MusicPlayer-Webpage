8ad01831e51951169a9f1b373accbc8e6e1de65e
1552980358 <1552980358@qq.com>
2021-12-19 14:08:15 +0800

    Update i18n files

--- src/i18n/lang/en.vue
+++ src/i18n/lang/en.vue
@@ -5,14 +5,20 @@ module.exports = {
   app: {
     'div_file_seq': 'Sequence',
     'div_file_name': 'File Name',
-    'div_file_size': 'File Size'
+    'div_file_size': 'File Size',
+    'div_file_status': 'Status',
+    'div_file_progress': 'Progress',
+    'progress_waiting': 'Waiting',
+    'progress_completed': 'Completed',
+    'progress_failed': 'Filed',
   },
 
   upload: {
     'text_drop_above': 'Release here to Select',
     'text_drop_empty': 'Click for selecting file(s) or dropping file(s) at here',
     'btn_upload': 'Upload',
-    'btn_remove': 'Remove All'
+    'btn_remove': 'Remove All',
+    'progress_uploading': 'Uploading',
   }
 
 }
--- src/i18n/lang/zh.vue
+++ src/i18n/lang/zh.vue
@@ -5,14 +5,20 @@ module.exports = {
   app: {
     'div_file_seq': '顺序',
     'div_file_name': '文件名',
-    'div_file_size': '文件大小'
+    'div_file_size': '文件大小',
+    'div_file_status': '状态',
+    'div_file_progress': '进度',
+    'progress_waiting': '等待中',
+    'progress_completed': '完成',
+    'progress_failed': '失败',
   },
 
   upload: {
     'text_drop_above': '释放文件以选中文件',
     'text_drop_empty': '点击选择文件或拖动文件到此处',
     'btn_upload': '上传',
-    'btn_remove': '删除全部'
+    'btn_remove': '删除全部',
+    'progress_uploading': '上传中',
   }
 
 }
