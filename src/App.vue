165f844b4cc9fcb494d495d086f3102757aba6de
1552980358 <1552980358@qq.com>
2022-05-20 19:11:39 +0800

    App.vue: Fix incorrect content margin and padding

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,8 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+html, body {
+  margin: 0 !important;
+  padding: 0 !important;
 }
 </style>
