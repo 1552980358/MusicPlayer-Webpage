b1321622ee83a0e9ca02a56dc1573a7c1a026f27
1552980358 <1552980358@qq.com>
2022-05-22 20:51:44 +0800

    SideBar.vue: Update SideBar component

---
+++ src/components/player/SideBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="side-bar-div-root">
+    <div class="side-bar-div-content-mask" v-on:click="close"></div>
+    <div class="side-bar-div-container">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  data() {
+    return {
+      openState: false,
+      barWidth: '0',
+    };
+  },
+  methods: {
+    updateState(newState) {
+      this.barWidth = (this.openState = newState ? '100vw' : '0vw');
+    },
+    open() {
+      this.updateState(true);
+    },
+    close() {
+      this.updateState(false);
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+.side-bar-div-root {
+  height: 100%;
+  width: v-bind(barWidth);
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  right: 0;
+  background-color: transparent;
+  overflow-x: hidden;
+  transition: 500ms ease;
+}
+
+.side-bar-div-content-mask {
+  float: left;
+  width: 75%;
+  height: 100%;
+}
+
+.side-bar-div-container {
+  float: right;
+  background-color: white;
+  width: 25%;
+  height: 100%;
+}
+
+</style>
