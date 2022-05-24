106f530f399050ec538363c756eae1fac684cf7f
1552980358 <1552980358@qq.com>
2022-05-24 21:23:09 +0800

    SideBar.vue: Implement playlist with audio list fetching

--- src/components/player/SideBar.vue
+++ src/components/player/SideBar.vue
@@ -2,13 +2,29 @@
   <div class="side-bar-div-root">
     <div class="side-bar-div-content-mask" v-on:click="close"></div>
     <div class="side-bar-div-container">
+      <div class="side-bar-div-playlist-container">
+        <div v-for="playlist in playlistList" :key="playlist.id">
+          <playlist-item :playlist="playlist" @openPlaylist="openPlaylist" />
+        </div>
+      </div>
+      <div class="side-bar-div-audio-container">
+        <div v-for="audio in audioList" :key="audio.id">
+          <audio-item :audio="audio" @play="play" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import AudioItem from "@/components/player/AudioItem";
+import PlaylistItem from "@/components/player/PlaylistItem";
+import {getResource} from "@/util/WebRequestUtil";
+
 export default {
   name: "side-bar",
+  props: [],
+  components: {PlaylistItem, AudioItem},
   data() {
     return {
       openState: false,
@@ -55,6 +71,27 @@ export default {
   background-color: white;
   width: 25%;
   height: 100%;
+  overflow: hidden;
+}
+
+.side-bar-div-playlist-container {
+  height: v-bind(playlistHeight);
+  width: 100%;
+  float: left;
+  overflow: hidden;
+
+  transition: height 400ms ease;
+  -webkit-transition: height 400ms ease;
+  -moz-transition: height 400ms ease;
+  -ms-transition: height 400ms ease;
+  -o-transition: height 400ms ease;
+}
+
+.side-bar-div-audio-container {
+  height: 100%;
+  width: 100%;
+  float: left;
+  background-color: white;
 }
 
 </style>
